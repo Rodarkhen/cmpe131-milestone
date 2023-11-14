61e188fe4b1de513392fe2e3fcdb21ab4ecc961a
Rodrigo Chen <rodrigo.chen@sjsu.edu>
2023-11-14 09:32:54 -0800

    Added starting files for the web develeopment

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+from markupsafe import escape
+from flask import render_template
+from app import myapp_obj
+@myapp_obj.route('/')
+@myapp_obj.route('/home')
+def home():
+    return render_template('home.html')
+
+@myapp_obj.route('/sign_up')
+def sign_up():
+    return render_template('sign_up.html')
+
+@myapp_obj.route('/create_note')
+def create_note():
+    return render_template('create_note.html')
+# Add more routes as needed
+
